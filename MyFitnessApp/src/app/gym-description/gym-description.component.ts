437a069257a60924c79b4cd2f70d40ab46994a6b
teodoraanton <antonteodora0@gmail.com>
2022-04-09 20:29:44 +0300

    Started to make gym description part

--- MyFitnessApp/src/app/gym-description/gym-description.component.ts
+++ MyFitnessApp/src/app/gym-description/gym-description.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gym-description',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gym-description.component.scss']
 })
 export class GymDescriptionComponent implements OnInit {
+
+  @Input() gymId: string = '';
 
   constructor() { }
 
--- MyFitnessApp/src/app/services/gym-description/gym-description.service.ts
+++ MyFitnessApp/src/app/services/gym-description/gym-description.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { GymDescription } from 'src/app/models/gymDescription';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class GymDescriptionService {
+  readonly baseUrl = 'https://localhost:5001';
+  readonly httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
 
-  constructor() { }
+  constructor(
+    private router: Router, 
+    private httpClient: HttpClient
+    ) { }
+
+    getGymDescriptionByGymID(gymID: String){
+      return this.httpClient.get<GymDescription>(
+        this.baseUrl + '/GymDescription/description/gymID/' + gymID,
+        this.httpOptions
+      );
+    }
 }
